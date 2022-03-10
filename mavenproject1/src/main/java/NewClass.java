bb92d2f877377296432238aefd52dd1e7bc65dcd
Papanatas95 <pablogarvia@hotmail.com>
2022-03-10 12:02:32 +0100

    Che para

---
+++ mavenproject1/src/main/java/NewClass.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Alumno
+ */
+public class NewClass {
+    <div class="col-4">
+            <p class="out">El Retrato de Lisa Gherardini, esposa de Francesco del Giocondo,1​ más conocido como La Gioconda (La Joconde en francés) o Monna Lisa, es una obra pictórica del polímata renacentista italiano Leonardo da Vinci. Fue adquirida por el rey Francisco
+                I de Francia a comienzos del siglo XVI y desde entonces es propiedad del Estado francés. Se halla expuesta en el Museo del Louvre de París, siendo, sin duda, la «joya».</p>
+            <img src="images/federico-scarionati-nkVa4ylaWG0-unsplash.jpg" alt="">
+            <div>
+                <button>Mas información</button></div>
+        </div>
+}
